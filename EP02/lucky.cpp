83023b5fdb72824ae456ec00a62a00e6d5f09ee8
Baileys <wulidan0530@live.com>
2020-07-05 23:49:17 +0800

    optively lucky dip

--- EP02/lucky.cpp
+++ EP02/lucky.cpp
@@ -3,7 +3,6 @@ using namespace std;
 const int N = 2e4+10;
 #define LL long long
 LL sum[N];
-double dp[N];
 int main() {
     int T, o = 0;
     cin >> T;
@@ -20,13 +19,13 @@ int main() {
         sum[n] = 0;
         for(int i = n-1;i >= 0;i--)
             sum[i] = sum[i+1] + a[i];
-        dp[0] = 1.0 * sum[0] / n;
+        double dp = 1.0 * sum[0] / n;
         for(int i = 1;i <= k;i++) {
-            // int id = upper_bound(a.begin(), a.end(), dp[i-1]) - a.begin();
-            int id = lower_bound(a.begin(), a.end(), dp[i-1]) - a.begin();
-            dp[i] = (id * dp[i-1] + sum[id]) / n;
+            int id = upper_bound(a.begin(), a.end(), dp) - a.begin();
+            // int id = lower_bound(a.begin(), a.end(), dp[i-1]) - a.begin();
+            dp = (id * dp + sum[id]) / n;
         }
-        printf("Case #%d: %.7f\n", ++o, dp[k]);
+        printf("Case #%d: %.7f\n", ++o, dp);
     }
     return 0;
 }
