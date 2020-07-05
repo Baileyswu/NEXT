7d181a303e9988ec1c3b8816c4a8cbcd46df6ae3
Baileys <wulidan0530@live.com>
2020-07-05 23:28:09 +0800

    lucky dip wa

---
+++ EP02/lucky.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 2e4+10;
+#define LL long long
+LL sum[N];
+double dp[N];
+int main() {
+    int T, o = 0;
+    cin >> T;
+    while(T--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a;
+        for(int i = 0;i < n;i++) {
+            int t;
+            cin >> t;
+            a.push_back(t);
+        }
+        sort(a.begin(), a.end());
+        sum[n] = 0;
+        for(int i = n-1;i >= 0;i--)
+            sum[i] = sum[i+1] + a[i];
+        dp[0] = 1.0 * sum[0] / n;
+        for(int i = 1;i <= k;i++) {
+            // int id = upper_bound(a.begin(), a.end(), dp[i-1]) - a.begin();
+            int id = lower_bound(a.begin(), a.end(), dp[i-1]) - a.begin();
+            dp[i] = (id * dp[i-1] + sum[id]) / n;
+        }
+        printf("Case #%d: %.7f\n", ++o, dp[k]);
+    }
+    return 0;
+}
