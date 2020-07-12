8ad09e76bf9cd6da121e4ebfaa956c9147df9e11
Baileyswu <wulidan0530@live.com>
2020-07-13 00:53:51 +0800

    dfs path is powerful

--- EP11/beauty_tree.cpp
+++ EP11/beauty_tree.cpp
@@ -2,48 +2,55 @@
 using namespace std;
 #define LL long long;
 const int N = 5e5+10;
-int pt[N];
-int son[N][2];
+int n, pt[N], gap[2], son[2][N];
+vector<int> tree[N], path;
 double f(double x, double y) {
     return 1-(1-x)*(1-y);
 }
-void have_son(int id, int gap, int s) {
-    int g = 0;
-    int t = id;
-    son[id][s]++;
-    while(id != pt[id]) {
-        id = pt[id];
-        g++;
-        if(g == gap) {
-            son[id][s]++;
-            g = 0;
-        }
+void init() {
+    path.clear();
+    memset(son, 0, sizeof(son));
+    for(int i = 0;i < N;i++)
+        tree[i].clear();
+}
+void dfs(int id) {
+    // printf("dfs %d\n", id);
+    path.push_back(id);
+    for(int i = 0;i < tree[id].size();i++) {
+        dfs(tree[id][i]);
     }
+    path.pop_back();
+
+    for(int i = 0;i < 2;i++) {
+        son[i][id]++;
+        int pre = path.size() - gap[i];
+        if(pre > -1) son[i][path[pre]] += son[i][id];
+    }
+    // printf("id = %d -- ", id);
+    // for(int i = 0;i < path.size();i++)
+        // printf("%d%c", path[i], " \n"[i==path.size()-1]);
 }
 int main() {
     int T, o = 0;
     cin >> T;
     while(T--) {
-        int n, a, b;
-        cin >> n >> a >> b;
+        init();
+        cin >> n >> gap[0] >> gap[1];
         pt[1] = 1;
         for(int i = 1;i < n;i++) {
             int t;
             cin >> t;
             pt[i+1] = t;
+            tree[t].push_back(i+1);
         }
-        memset(son, 0, sizeof(son));
-        for(int i = 1;i <= n;i++) {
-            have_son(i, a, 0);
-            have_son(i, b, 1);
-        }
-        // for(int i = 1;i <= n;i++)
-        //     printf("%d%c", son[i][0], " \n"[i==n]);
-        // for(int i = 1;i <= n;i++)
-        //     printf("%d%c", son[i][0], " \n"[i==n]);
+        dfs(1);
         double ans = 0;
-        for(int i = 1;i <= n;i++)
-            ans += f(1.0*son[i][0]/n, 1.0*son[i][1]/n);
+        // for(int i = 1;i <= n;i++)
+            // printf("%d%c", son[0][i], " \n"[i==n]);
+        // for(int i = 1;i <= n;i++)
+            // printf("%d%c", son[1][i], " \n"[i==n]);
+        for(int i = 1;i <= n;i++) 
+            ans += f(1.0*son[0][i]/n, 1.0*son[1][i]/n);
         printf("Case #%d: %.7f\n", ++o, ans);
     }
     return 0;
