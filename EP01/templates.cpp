4082e93aab43f3ffac05bc9e1dad21617476aa2f
Baileyswu <wulidan0530@live.com>
2020-07-13 15:42:46 +0800

    easy doors

---
+++ EP01/templates.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LL long long
+const int N = 1;
+int main() {
+    int T, o = 0;
+    cin >> T;
+    while(T--) {
+        printf("Case #%d: \n", ++o);
+    }
+    return 0;
+}
